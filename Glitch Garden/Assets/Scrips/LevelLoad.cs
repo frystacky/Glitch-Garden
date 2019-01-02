080db800cc32e48200dbb2d3571c8abc3b49c879
Daniel Frystacky <danielfrystacky@gmail.com>
2019-01-02 15:27:41 -0500

    created new project, added background and basics assets

---
+++ Glitch Garden/Assets/Scrips/LevelLoad.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int loadTime = 3;
+    int currentSceneIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;       //gets current scene from build index
+
+        //load time for splash screen
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(loadTime);
+        LoadNextScene();
+
+    }
+
+    //loads next scene
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
