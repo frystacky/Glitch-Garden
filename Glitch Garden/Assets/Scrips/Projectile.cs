167557d64f63865987522eec187ccd2535488481
Daniel Frystacky <danielfrystacky@gmail.com>
2019-01-04 15:51:41 -0500

    Added spawner, projectiles for defender, health and damge

---
+++ Glitch Garden/Assets/Scrips/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1f;
+    [SerializeField] float damge = 50f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        //Debug.Log("I hit: " + othercollision.name);
+
+        health.DealDamage(damge);
+    }
+}
