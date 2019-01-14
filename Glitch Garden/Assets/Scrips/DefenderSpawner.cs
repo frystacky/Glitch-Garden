1a1b12529ad034b6344e9ada68b76e008f8a1022
Daniel Frystacky <danielfrystacky@gmail.com>
2019-01-14 12:44:54 -0500

    Change the Child level in animation in our attackers and defenders and trophy now genrates currency

--- Glitch Garden/Assets/Scrips/DefenderSpawner.cs
+++ Glitch Garden/Assets/Scrips/DefenderSpawner.cs
@@ -39,6 +39,6 @@ public class DefenderSpawner : MonoBehaviour
     private void SpawnDefender(Vector2 roundedPos)
     {
         Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
-        Debug.Log(roundedPos);
+        //Debug.Log(roundedPos);
     }
 }
